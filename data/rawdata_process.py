d08f922e3c74b470ac38715b3d16065b9294ee63
zwc662 <zwc662@gmail.com>
2018-06-19 17:48:03 -0400

    update

--- data/rawdata_process.py
+++ data/rawdata_process.py
@@ -1,14 +1,11 @@
 import numpy as np
-import h5py
 import json
+import preprocess
+import scipy.io
 
-f = h5py.File('./DataSet.mat', 'r')
+f = scipy.io.loadmat('TrainSet.mat')
 data = f.get('data')
 data = np.mat(data)
-action = f.get('Car1_act')
-action = np.array(action)
-no_car_dis = 500
-no_car_v = -30
 tra = []
 
 l_p_1 = []
@@ -19,6 +16,9 @@ v_2 = []
 l_p_3 = []
 d_3 = []
 v_3 = []
+a_1 = []
+a_2 = []
+a_3 = []
 
 def lane_position(pos):
     if pos < -41.0:
@@ -26,10 +26,11 @@ def lane_position(pos):
     else:
         return 1
 
-for t in range(0, int(data.shape[1]/3)):
-    car1 = data[:, 3*t].reshape((1, data.shape[0])).tolist()[0]
-    car2 = data[:, 3*t+1].reshape((1, data.shape[0])).tolist()[0]
-    car3 = data[:, 3*t+2].reshape((1, data.shape[0])).tolist()[0]
+print(data.shape)
+for t in range(0, int(data.shape[0]/3)):
+    car1 = data[3*t, :].reshape((1, data.shape[1])).tolist()[0]
+    car2 = data[3*t+1, :].reshape((1, data.shape[1])).tolist()[0]
+    car3 = data[3*t+2, :].reshape((1, data.shape[1])).tolist()[0]
     
     tra.append(car1[2])
     
@@ -45,6 +46,9 @@ for t in range(0, int(data.shape[1]/3)):
     v_2.append(car2[3] - car1[3])
     v_3.append(car3[3] - car1[3])
 
+    a_1.append(car1[4])
+    a_2.append(car2[4])
+    a_3.append(car3[4])
     
     if t == 1:
         print(tra[-1])
@@ -56,20 +60,25 @@ for t in range(0, int(data.shape[1]/3)):
         print(v_3[-1])
         print(d_2[-1])
         print(d_3[-1])
+        print(a_1[-1])
 
 state_data ={
     "Car1_Position": tra,
     "Car1_Lane_Position": l_p_1,
     "Car1_Velocity": v_1,
+    "Car1_Action": a_1,
     "Car2_Lane_Position": l_p_2,
     "Car2_Distance": d_2,
     "Car2_Velocity": v_2,
+    "Car2_Action": a_2,
     "Car3_Lane_Position": l_p_3,
     "Car3_Distance": d_3,
-    "Car3_Velocity": v_3,}
+    "Car3_Velocity": v_3,
+    "Car3_Action": a_3
+}   
 #"Action": action.tolist}
 
-with open('data_v1.json', 'w') as fp:
+with open('data.json', 'w') as fp:
     json.dump(state_data, fp)
 
- 
+preprocess.preprocess_list(path = './data.json')
